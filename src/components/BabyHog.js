e34feb1f9dd303330698291db8b13c3fa0c9c622
tkoar <terrancekoar@gmail.com>
2017-10-30 17:07:56 -0400

    making bby hogs change size

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -10,7 +10,19 @@ class BabyHog extends Component {
       id: props.id,
       name: props.name,
       weight: props.weight,
-      eyeColor: props.eyeColor
+      eyeColor: props.eyeColor,
+      imgHeight: 200
+    }
+  }
+
+  changeWeight(event) {
+    event.preventDefault()
+    let action = event.target.id
+    if (action === 'increase') {
+      this.setState({imgHeight: `${this.state.imgHeight += 20}`})
+    }
+    if (action === 'decrease') {
+      this.setState({imgHeight: `${this.state.imgHeight -= 20}`})
     }
   }
 
@@ -20,16 +32,16 @@ class BabyHog extends Component {
           <h1>Name: {this.state.name}</h1>
           <h3>Weight: {this.state.weight}</h3>
           <h4>Eye Color: {this.state.eyeColor}</h4>
-          <img src={Baby} style={{height: 200}} alt="MasterBlasterJrJr" />
+          <img src={Baby} style={{height: `${this.state.imgHeight}`}} alt="MasterBlasterJrJr" />
 
-          <Button animated value={0.5}>
-            <Button.Content visible>Increase Weight</Button.Content>
-            <Button.Content hidden>
-              <Icon name='plus' />
+          <Button animated id="increase" onClick={this.changeWeight.bind(this)}>
+            <Button.Content id="increase" visible>Increase Weight</Button.Content>
+            <Button.Content id="increase" hidden>
+              <Icon id="increase" name='plus' />
             </Button.Content>
           </Button>
 
-          <Button animated value={-0.5}>
+          <Button animated value={-20} onClick={this.changeWeight}>
             <Button.Content visible>Decrease Weight</Button.Content>
             <Button.Content hidden>
               <Icon name='minus' />
